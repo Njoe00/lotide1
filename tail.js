bec8b0e59d40a0b73aeeed266ad8b14efeb0b188
vagrant <vagrant@homestead.test>
2022-11-15 02:24:33 +0000

    created tail function that returns all elements of the first but the first

---
+++ tail.js
@@ -0,0 +1,28 @@
+const tail = function(arr) {
+  let result = [];
+  result = arr.slice(1);
+  return result;
+};
+
+
+
+
+
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
