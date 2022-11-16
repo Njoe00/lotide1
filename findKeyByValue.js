6a4c412b26d48bf1eee54cbbe46f66f264b52fd6
vagrant <vagrant@homestead.test>
2022-11-16 23:14:15 +0000

    created function that takes in an object & key. Compares key pair values inside the object and returns the key when a the inputed value is matched with a key pair.

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+const findKeyByValue = function(obj, val) {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
