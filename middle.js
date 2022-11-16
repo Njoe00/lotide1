3d68716af8192175ead7e4cbc41a9d24c83f0345
vagrant <vagrant@homestead.test>
2022-11-16 19:34:35 +0000

    updated to clean up code

--- middle.js
+++ middle.js
@@ -44,7 +44,4 @@ assertArraysEqual(middle([1]),([]));
 assertArraysEqual(middle([1, 2]), ([]));
 assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
 assertArraysEqual(middle([1, 2, 3, 4, 5]),(3));
-
-  
-//   assertArraysEqual([1, 2, 3], [1, 2, 3]);
   
