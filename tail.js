5cbfb416735eea58e974b71419200cce93393114
Njoe00 <nick.james.joe@gmail.com>
2022-11-21 22:18:21 +0000

    Refactored tail code to using module.exports and require

--- tail.js
+++ tail.js
@@ -1,28 +1,9 @@
+const assertEqual = require("./assertEqual");
+
 const tail = function(arr) {
   let result = [];
   result = arr.slice(1);
   return result;
 };
 
-
-
-
-
-
-
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-  
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
