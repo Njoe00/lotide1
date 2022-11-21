2b302ba29627c05a280084bf6e8fc4e916b9d668
Njoe00 <nick.james.joe@gmail.com>
2022-11-21 22:08:31 +0000

    refactored code to use module.exports and require to call functions from different files

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,4 @@ const assertEqual = function(actual, expected) {
   return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,25 +1,9 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   for (let i = 0; i < arr.length; i++) {
     return arr[0];
   }
 };
 
-
-
-
-
-
-
-
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-  
-assertEqual(head([5,6,7]),5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
